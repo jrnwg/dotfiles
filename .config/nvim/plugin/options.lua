db33650ef088af5f97736774994749f8ffdf1e2b
jrnwg <189479406+jrnwg@users.noreply.github.com>
2025-07-31 13:14:47 +0200

    nvim: options

--- .config/nvim/plugin/options.lua
+++ .config/nvim/plugin/options.lua
@@ -1,14 +1,25 @@
-local opt = vim.opt
+vim.opt.termguicolors = true
 
-opt.termguicolors = true
+vim.opt.mouse = "a"
 
-opt.mouse = "a"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 10
 
-opt.number = true
-opt.relativenumber = true
-opt.cursorline = true
-opt.scrolloff = 10
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.inccommand = "split"
 
-opt.ignorecase = true
-opt.smartcase = true
-opt.inccommand = "split"
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv "HOME" .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.autoread = true
+vim.opt.autowrite = false
+
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "88"
