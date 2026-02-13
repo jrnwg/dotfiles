5a558434e0204269ffcec809daa22c47574fc1ae
jrnwg <189479406+jrnwg@users.noreply.github.com>
2026-02-13 14:11:50 +0100

    follow system colors

--- .config/fzf/colors.sh
+++ .config/fzf/colors.sh
@@ -1,39 +1,23 @@
-# Scheme name: Nord
-# Scheme system: base16
-# Scheme author: arcticicestudio
-# Template author: Tinted Theming (https://github.com/tinted-theming)
-
-# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
-# " --color=bg+:#3b4252,bg:#2e3440,spinner:#88c0d0,hl:#81a1c1"\
-# " --color=fg:#d8dee9,header:#81a1c1,info:#ebcb8b,pointer:#88c0d0"\
-# " --color=marker:#88c0d0,fg+:#eceff4,prompt:#ebcb8b,hl+:#81a1c1"
-
-# Tokyonight Moon
-#
-# based on
-# https://github.com/folke/tokyonight.nvim/blob/main/extras/fzf/tokyonight_moon.sh
-# https://github.com/folke/tokyonight.nvim/blob/main/extras/ghostty/tokyonight_moon
-
 export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
   --highlight-line \
   --info=inline-right \
   --ansi \
   --layout=reverse \
   --border=none \
-  --color=bg+:#2d3f76 \
-  --color=bg:#222436 \
-  --color=border:#82aaff \
-  --color=fg:#c8d3f5 \
-  --color=gutter:#1b1d2b \
-  --color=header:#ffc777 \
-  --color=hl+:#86e1fc \
-  --color=hl:#86e1fc \
-  --color=info:#828bb8 \
-  --color=marker:#ff757f \
-  --color=pointer:#ff757f \
-  --color=prompt:#82aaff \
-  --color=query:#c8d3f5:regular \
-  --color=scrollbar:#444a73 \
-  --color=separator:#ffc777 \
-  --color=spinner:#ff757f \
+  --color=bg+:bright-black \
+  --color=bg:-1 \
+  --color=border:blue \
+  --color=fg:-1 \
+  --color=gutter:-1 \
+  --color=header:yellow \
+  --color=hl+:cyan \
+  --color=hl:cyan \
+  --color=info:bright-black \
+  --color=marker:red \
+  --color=pointer:red \
+  --color=prompt:blue \
+  --color=query:7 \
+  --color=scrollbar:-1 \
+  --color=separator:yellow \
+  --color=spinner:red \
 "
